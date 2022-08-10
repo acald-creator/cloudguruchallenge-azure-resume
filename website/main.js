39794a85c3b516c491b4d5777a0699a0e9584fc6
Antonette Caldwell <pullmana8@gmail.com>
2022-08-10 12:13:36 -0500

    Update website and javascript

--- website/main.js
+++ website/main.js
@@ -1,31 +1,24 @@
 const localFuncApi = "http://localhost:7071/api/GetVisitorCounter"
 
 function visitorCount() {
-    let counter = document.getElementById('counter').innerText;
-    let count = 0;
-    count = parseInt(counter.innerText);
-    counter.innerHTML = count;
+  let counter = document.getElementById('counter').innerText;
+  let count = 0;
+  count = parseInt(counter.innerText);
+  counter.innerHTML = count;
 }
 
 window.onload = visitorCount();
 
-const params = {
-    method: 'GET',
-    headers: {
-        'accept': 'application/json'
-    },
-    mode: "no-cors",
-};
+async function main() {
+  const response = await fetch("https://resume.acaldwell.dev/resume.json", {
+    method: "GET"
+  });
 
-let URL = "https://resume.acaldwell.dev/resume.json";
+  if (response.ok) {
+    let json = await response.json();
+  } else {
+    console.log("HTTP-Error: " + response.status);
+  }
+}
 
-fetch(URL, params)
-    .then(function (response) {
-        return response.text()
-    })
-    .then(function (data) {
-        Promise.resolve(data ? JSON.parse(data) : {})
-    })
-    .catch(function (error) {
-        return console.log(error)
-    })
+main();
